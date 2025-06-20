97df7c90623c6fa420c03b98dee0e079459bc8e3
BlackShark <Bl4ckSh4rk@users.noreply.github.com>
2025-06-20 17:11:32 +0200

    Fixed abstract

--- PKHeX.Core/Saves/SAV3.cs
+++ PKHeX.Core/Saves/SAV3.cs
@@ -642,7 +642,7 @@ public abstract class SAV3 : SaveFile, ILangDeviantSave, IEventFlag37, IBoxDetai
     }
 
     public abstract int TrainerRematchSteps { get; set; }
-    public abstract Span<byte> TrainerRematches { get; set; }
+    public abstract Span<byte> TrainerRematches { get; }
 
     #region eBerry
     public abstract Span<byte> EReaderBerry();
